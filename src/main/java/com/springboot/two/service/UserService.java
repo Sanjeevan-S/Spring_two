84968cffa7f69c6a6d31d85966e3133fc6e9b65b
Sanjeevan <snjvrocks@gmail.com>
2023-01-21 10:06:03 +0530

    User Service class used to perform CRUD operations

--- src/main/java/com/springboot/two/service/UserService.java
+++ src/main/java/com/springboot/two/service/UserService.java
@@ -1,9 +1,45 @@
 package com.springboot.two.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.two.model.User;
+import com.springboot.two.repository.UserRepository;
+
+@Service
 public class UserService {
 
-	public UserService() {
-		// TODO Auto-generated constructor stub
+	@Autowired
+	private UserRepository userRepository;
+	
+	public Object createUser(User reqData) {
+		return userRepository.save(reqData);
+	}
+	
+	public Object getAllUsers() {
+		return userRepository.findAll();
+	}
+	
+	public User isDataExist(User reqData) {
+		return userRepository.findByEmailAndMobNo(reqData.getEmail(), reqData.getMobNo());	
+	}
+	
+	public Object getUserById(Long id) {
+		return userRepository.findById(id);
+	}
+	
+	public Object updateUser(User reqData, User isData) {
+		isData.setName(reqData.getName());
+		isData.setEmail(reqData.getEmail());
+		isData.setMobNo(reqData.getMobNo());
+		isData.setPassword(reqData.getPassword());
+		return userRepository.save(isData);
 	}
 
+	public Object deleteUserbyId(Long id) {
+		userRepository.deleteById(id);
+		return null;
+	}
 }
+
+
